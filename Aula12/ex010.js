729f4a76e07ba6c56b8e1de1eebb727592912afa
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-08-02 20:10:24 -0300

    Create ex010.js

---
+++ Aula12/ex010.js
@@ -0,0 +1,13 @@
+var idade = 55
+if(idade < 16) {
+    console.log ('Nao vota')
+} else 
+    if(idade >= 16 && idade <= 18) {
+        console.log ('Voto opicional')
+    }
+    if(idade >= 19 && idade < 60) {
+        console.log ('Vota')
+    }
+    if(idade >= 60) {
+        console.log ('Vota opicional')
+    }
