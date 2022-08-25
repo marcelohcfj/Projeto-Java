caf3c955241f0e1edbe3f944fe30ce392310ad3f
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-08-25 20:02:04 -0300

    .

--- Aula12ex/ex015/script1.js
+++ Aula12ex/ex015/script1.js
@@ -8,7 +8,44 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var  img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'bebehomem.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovemhomem.jpeg')
+            } else if (idade < 50){
+                //Aduto
+                img.setAttribute('src', 'adultohomem.jpeg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosohomem.jpeg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'bebemulher.jpg')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovemmulher.jpeg')
+            } else if (idade < 50){
+                //Aduto
+                img.setAttribute('src', 'adultomulher.jpeg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosomulher.jpeg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.style.fontSize = '20pt'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img )
     }
 }
  
