1194175f0a1e4adafe00a1f677093b6f2b084ab5
Marcelo Henrique <93296406+marcelohcfj@users.noreply.github.com>
2022-09-06 19:54:49 -0300

    .

--- Aula16ex/Ex018/scripttt.js
+++ Aula16ex/Ex018/scripttt.js
@@ -20,12 +20,9 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('tudo ok')
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
-} 
+}
